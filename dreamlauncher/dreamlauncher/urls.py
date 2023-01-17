4592d7556419246418f47c1ff419b5e4f1215455
amr gomaa <amr.gomaa966@gmail.com>
2023-01-17 23:55:55 +0200

    add login/register

--- dreamlauncher/dreamlauncher/urls.py
+++ dreamlauncher/dreamlauncher/urls.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import core.views as cv 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    #path("", cv.homepage, name="homepage"),
     
+    path("register", cv.register_request, name="register")
 ]
+    
+
+
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+ 
+
+
+
+    
